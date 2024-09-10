f9037ea20a7db0d7a1ed5e82a097afc1943a53e5
Abdessamad FAKIR <abdessamadfakir8@gmail.com>
2024-09-10 22:15:10 +0200

    modifier build projet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideIonicAngular} from '@ionic/angular/standalone';
 import {provideHttpClient} from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideIonicAngular({}), provideHttpClient()]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideIonicAngular({}), provideHttpClient(), provideClientHydration()]
 };
